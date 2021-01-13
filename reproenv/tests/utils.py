dd8bc893a04ac65f5774138ab9509475479e1802
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-01-13 17:32:28 -0500

    skip docker tests if dockerpy or docker engine not available

--- reproenv/tests/utils.py
+++ reproenv/tests/utils.py
@@ -1,4 +1,5 @@
 import subprocess
+
 import pytest
 
 try:
@@ -6,15 +7,29 @@ try:
 except ImportError:
     docker = None
 
+
+def _docker_available():
+    """Return `True` if docker-py is installed and docker engine is available."""
+    if docker is None:
+        return False
+    client = docker.from_env()
+    try:
+        return client.ping()  # bool, unless engine is unresponsive (eg not installed)
+    except docker.errors.APIError:
+        return False
+
+
+def _singularity_available():
+    process = subprocess.run("sudo singularity help".split())
+    return process.returncode == 0
+
+
 # See https://docs.pytest.org/en/stable/skipping.html#id1 for skipif.
 
 skip_if_no_docker = pytest.mark.skipif(
-    docker is None, reason="docker python package not found"
+    not _docker_available(), reason="docker not available"
 )
 
-process = subprocess.run("sudo singularity help".split())
-singularity_available = process.returncode == 0
-
 skip_if_no_singularity = pytest.mark.skipif(
-    not singularity_available, reason="singularity not found"
+    not _singularity_available(), reason="singularity not available"
 )
